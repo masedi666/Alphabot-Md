83ecf6cbc196992dc1eeae7e6852a046a32fe591
masedi666 <129651980+masedi666@users.noreply.github.com>
2023-04-04 06:31:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,22 +6,18 @@ const {
    spanyol
 } = require(`./language`)
 
-//sebagian panel gk support opena ai jadi mungkin apikey lu bakalan di blokir
-//klo misalkan pada saat memakai fitur ai, terus dapat email dari open si, itu berarti website tempat lu install bot gk support ai
-global.OpenAIKey = "YOUR_KEY" //login to https://platform.openai.com/account/api-keys to get apikey
-
+global.OpenAIKey = "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" //login to https://platform.openai.com/account/api-keys to get apikey
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
-   alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
+   alfa: 'https://edikurbotapi.herokuapp.com', //apabila link api eror, segera laporkan ke owner
 }
 
 //buy apikey premium 0887435047326
 // Free apikey (silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.my.id to get apikey
+   'https://edikurbotapi.herokuapp.com': 'Abmt9WABjS', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
-global.sewabotnya = `
-edit sewabot di file settings.js line 24
+global.sewabotnya = `SEWA BOT HANYA 5K/BLN HUBUNGI OWNER
 `
 // setting 
 global.firtsChat = true //send firts message to new user
@@ -29,13 +25,11 @@ global.autoread = true // auto read message
 global.antidelete = false // anti delete
 global.antiviewonce = true // anti view once
 global.autobio = false //auto ganti bio
-global.anticall = true //anti call, jika true maka org yg nelpon bot auto di blokir
+global.anticall = true //anti call 
 global.userRegister = false
 global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 global.autolevelup = true
-global.autowelcome = true //auto welcome msg all group
-global.autogoodbye = true //auto goodbye msg all group
-global.groupOnly = false //bot hanya bisa di gunakan dalam grup jika true
+global.groupOnly = false //bot hanya bisa di gunakan dalam grup
 
 //language
 //Available in indonesia , english & spanyol 
@@ -46,20 +40,20 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Edikurbot-Mdོ" //namabot kalian
+global.ownername = "Edikur.exe" //nama kalian
+global.myweb = "https://edikurexe.my.id" //bebas asal jan hapus
+global.youtube = "https://instagram.com/edikur.exe" //bebas asal jan hapus
+global.myweb2 = "https://edikurexe.my.id" //bebas
+global.email = "edikur.exe@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.premium = ['6285155282285'] //premium user
+global.owner = ["6285155282285"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285155282285" // nomor wa kalian
+global.ownernomerr = "+6285155282285"+ownernomer //nmr wa kalian
+global.thumbnail = "./image/1673722600948.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/qris.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
 global.packname = '© ' + botname //sticker wm ubah
@@ -68,7 +62,7 @@ global.sessionName = 'session'
 //type menu for menu
 global.typemenu = 'templateImage' //
 //type menu for downloadmenu, animemenu, etc
-global.typemenu2 = 'image' //
+global.typemenu2 = 'templateImage' //
 global.multiplier = 69
 global.limitawal = {
    premium: "Infinity", //unlimited
@@ -76,7 +70,7 @@ global.limitawal = {
 }
 global.buttonDefault = [{
    urlButton: {
-      displayText: 'Youtube Channel',
+      displayText: 'Instagram',
       url: youtube
    }
 }, {
